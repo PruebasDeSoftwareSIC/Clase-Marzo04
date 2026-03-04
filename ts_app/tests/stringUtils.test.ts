d848a9ecd5d2b49d282887cb3f59b570fc5d5eb7
Santiago Idárraga Ceballos <san200526@gmail.com>
2026-03-04 10:28:11 -0500

    feat(test): define casos de prueba para palindrome

--- ts_app/tests/stringUtils.test.ts
+++ ts_app/tests/stringUtils.test.ts
@@ -1,11 +1,25 @@
-
 import { describe, it, expect } from 'vitest'
 import { isPalindrome } from '../src/stringUtils'
 
 // TODO: completa los casos: "radar"->true, "anita lava la tina"->true, "python"->false, ""->true, "Radar"->true
 
-describe('siembra', () => {
-  it('ejemplo', () => {
-    expect(true).toBe(true)
-  })
-})
+describe('test isPalindrome es true', () => {
+  it('radar es palindromo', () => {
+    expect(isPalindrome('radar')).toBe(true)
+  });
+  it('anita lava la tina es palindromo', () => {
+    expect(isPalindrome('anita lava la tina')).toBe(true)
+  });
+  it('cadena vacía es palindromo', () => {
+    expect(isPalindrome('')).toBe(true)
+  });
+  it('Radar es palindromo (ignora mayúsculas)', () => {
+    expect(isPalindrome('Radar')).toBe(true)
+  });
+});
+
+describe('test isPalindrome es false', () => {
+  it('python no es palindromo', () => {
+    expect(isPalindrome('python')).toBe(false)
+  });
+});
